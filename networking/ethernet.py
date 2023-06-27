3704dd4298e3a9d899ae31aad5703578b6361465
Gustavo Gobbato <gusgobbato10@hotmail.com.br>
2023-06-26 22:40:52 -0300

    Organização e adição da captura IPv6.

--- networking/ethernet.py
+++ networking/ethernet.py
@@ -1,4 +1,3 @@
-import socket
 import struct
 from general import *
 
@@ -11,8 +10,5 @@ class Ethernet:
 
         self.dest_mac = get_mac_addr(dest)
         self.src_mac = get_mac_addr(src)
-        self.proto = socket.htons(prototype)
+        self.proto = int(prototype)
         self.data = raw_data[14:]
-
-
-
